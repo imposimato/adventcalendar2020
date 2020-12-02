6de12afa3fd33163c83dd1f3b5b72a89d0222f84
Luiz Imposimato Jr <luiz_1982@hotmail.com>
2020-12-02 19:52:31 +0000

    Improvements

--- 02/02.js
+++ 02/02.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
-const inputs = fs.readFileSync('02.txt').toString().split('\n');
+const inputs = fs.readFileSync('02.txt')
+    .toString().split('\n')
+    .filter(v => !!v);
 
 let resultPolicyOne = 0;
 let resultPolicyTwo = 0;
@@ -8,20 +10,21 @@ for (const line of inputs) {
     const res = line.split(': ');
     // Extract patern and key;
     const paternAndKey = res[0].split(' ');
-    const repetition = paternAndKey[0].split('-').map(v => parseInt(v));
+    const repetition = paternAndKey[0]
+        .split('-')
+        .map(v => parseInt(v));
+
     const key = paternAndKey[1];
     const password = res[1];
     let countChars = 0;
-
-    if (password) {
-        for (const c of password) {
-            if (c && c.toString() === key) {
-                countChars++;
-            }
+    for (const c of password) {
+        if (c && c.toString() === key) {
+            countChars++;
         }
     }
+
     countChars >= repetition[0] && countChars <= repetition[1] && resultPolicyOne++;
-    password && (password[repetition[0] - 1] === key) ^ (password[repetition[1] - 1] === key) && resultPolicyTwo++;
+    (password[repetition[0] - 1] === key) ^ (password[repetition[1] - 1] === key) && resultPolicyTwo++;
 }
 
 console.log(resultPolicyOne);
